41eac8376805dd21af0476a786128aaaa9241b4a
GintMist <themistghillein@gmail.com>
2017-03-30 17:54:11 +0300

    visual improvements

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,7 +6,9 @@ import Graphics.Gloss.Geometry.Angle
 data Pendulum = Pendulum { mass   :: Float
                          , length :: Float
                          , theta  :: Float
-                         , omega  :: Float } deriving (Show, Eq)
+                         , omega  :: Float
+                         , oldPos :: (Float, Float)
+                         , path   :: [Picture] } deriving (Show, Eq)
 
 data DoublePendulumSystem = DPS { pendulum1 :: Pendulum
                                 , pendulum2 :: Pendulum
@@ -15,7 +17,7 @@ data DoublePendulumSystem = DPS { pendulum1 :: Pendulum
                                 , dt        :: Float } deriving (Show, Eq)
 
 derivatives :: DoublePendulumSystem -> [Float]
-derivatives (DPS (Pendulum m1 l1 theta1 omega1) (Pendulum m2 l2 theta2 omega2) g _ _) = [theta1', omega1', theta2', omega2']
+derivatives (DPS (Pendulum m1 l1 theta1 omega1 _ _) (Pendulum m2 l2 theta2 omega2 _ _) g _ _) = [theta1', omega1', theta2', omega2']
     where 
         theta1' = omega1
         theta2' = omega2
@@ -37,8 +39,8 @@ derivatives (DPS (Pendulum m1 l1 theta1 omega1) (Pendulum m2 l2 theta2 omega2) g
 rk4PendulumSolver :: DoublePendulumSystem -> DoublePendulumSystem
 rk4PendulumSolver ps = mkps yo
     where
-        (DPS (Pendulum m1 l1 theta1 omega1) (Pendulum m2 l2 theta2 omega2) g t dt) = ps
-        mkps [a,b,c,d] = DPS (Pendulum m1 l1 (normalizeAngle a) b) (Pendulum m2 l2 (normalizeAngle c) d) g (t + dt) dt
+        (DPS (Pendulum m1 l1 theta1 omega1 (ox1, oy1) p1) (Pendulum m2 l2 theta2 omega2 (ox2, oy2) p2) g t dt) = ps
+        mkps [a,b,c,d] = DPS (Pendulum m1 l1 (normalizeAngle a) b (nx1, ny1) np1) (Pendulum m2 l2 (normalizeAngle c) d (nx2, ny2) np2) g (t + dt) dt
         yi = [theta1, omega1, theta2, omega2]
         k1 = map (* dt) (derivatives ps)
         k2 = map (* dt) (derivatives (mkps $ zipWith (+) yi (map (* 0.5) k1)))
@@ -48,14 +50,21 @@ rk4PendulumSolver ps = mkps yo
             (zipWith (+) (map (/ 6) k1) 
             (zipWith (+) (map (/ 3) k2) 
             (zipWith (+) (map (/ 3) k3) (map (/ 6) k4))))             
+        n = (+) (3*pi/2)
+        nx1 = cos (n theta1) * l1 * 100
+        ny1 = sin (n theta1) * l1 * 100
+        nx2 = nx1 + (cos (n theta2) * l2) * 100
+        ny2 = ny1 + (sin (n theta2) * l2) * 100
+        np1 = color cyan (line [(ox1, oy1), (nx1, ny1)]) : p1
+        np2 = color magenta (line [(ox2, oy2), (nx2, ny2)]) : p2
 
 dpsdrawer :: DoublePendulumSystem -> Picture
-dpsdrawer (DPS (Pendulum m1 l1 t1 _) (Pendulum m2 l2 t2 _) _ _ _) = 
-    pictures [ line [(0, 0), (a, b)]
-             , line [(a, b), (c, d)]
-             , color red $ circleSolid 10
-             , translate a b $ color blue $ circleSolid (5*m1)
-             , translate c d $ color blue $ circleSolid (5*m2) ]
+dpsdrawer (DPS (Pendulum m1 l1 t1 _ _ p1) (Pendulum m2 l2 t2 _ _ p2) _ _ _) = 
+    pictures ([ color white $ line [(0, 0), (a, b)]
+              , color white $ line [(a, b), (c, d)]
+              , color yellow $ circleSolid 3
+              , translate a b $ color orange $ circleSolid (5*m1)
+              , translate c d $ color orange $ circleSolid (5*m2) ] ++ take 200 p1 ++ take 200 p2)
     where
         n = (+) (3*pi/2)
         a = cos (n t1) * l1 * 100
@@ -68,8 +77,14 @@ fps = 200
 
 anim [m1, l1, t1, o1, m2, l2, t2, o2] = 
     simulate (InWindow "DPS" (ceiling $ (l1 + l2) * 220, ceiling $ (l1 + l2) * 220) (10, 10)) 
-             white 
+             black 
              fps 
-             (DPS (Pendulum m1 l1 t1 o1) (Pendulum m2 l2 t2 o2) 9.81 0 0.005) 
+             (DPS (Pendulum m1 l1 t1 o1 (nx1,ny1) []) (Pendulum m2 l2 t2 o2 (nx2,ny2) []) 9.81 0 0.005) 
              dpsdrawer 
              (\_ _ -> rk4PendulumSolver)
+    where
+        n = (+) (3*pi/2)
+        nx1 = cos (n t1) * l1 * 100
+        ny1 = sin (n t1) * l1 * 100
+        nx2 = nx1 + (cos (n t2) * l2) * 100
+        ny2 = ny1 + (sin (n t2) * l2) * 100
